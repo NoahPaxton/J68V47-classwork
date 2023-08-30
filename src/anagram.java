ceb4539afc3419090bc399ee9e141c0484f38dba
NoahPaxton <143596717+NoahPaxton@users.noreply.github.com>
2023-08-30 12:24:57 +0100

    Update anagram.java

--- src/anagram.java
+++ src/anagram.java
@@ -1,7 +1,7 @@
 public class anagram {
     public static void main(String[] args) {
-        System.out.println("dr owl = world");
-        System.out.println("fell wear = farewell");
+        System.out.println("atom invito = motovation");
+        System.out.println("coin trusted = destruction");
         System.out.println("chrome pensive = comprehensive");
         System.out.println("arm contempt = compartment");
         System.out.println("Antigone visit = investigation");
