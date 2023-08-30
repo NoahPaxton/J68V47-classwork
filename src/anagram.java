98dcd1eeb73c4eb5ccfb59df4c87df7721d3e332
Noah Paxton <30115747@nescol.ac.uk>
2023-08-30 11:52:25 +0100

    test

---
+++ src/anagram.java
@@ -0,0 +1,10 @@
+public class anagram {
+    public static void main(String[] args) {
+        System.out.println("dr owl = world");
+        System.out.println("fell wear = farewell");
+        System.out.println("chrome pensive = comprehensive");
+        System.out.println("arm contempt = compartment");
+        System.out.println("Antigone visit = investigation");
+
+    }
+}
