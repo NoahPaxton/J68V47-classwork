9de90e51fc231034f9154e19c02545d4e34c7700
Noah Paxton <30115747@nescol.ac.uk>
2023-10-04 10:10:07 +0100

    new file

---
+++ src/even.java
@@ -0,0 +1,11 @@
+public class even {
+    public static void main(String[] args) {
+        int number = 10;
+
+        while( number<=24 )
+        {
+            System.out.println(number);
+            number +=2;
+        }
+    }
+}
