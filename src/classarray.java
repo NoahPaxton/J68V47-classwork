d924dee75b680eec6d90e988213629da1470ada9
Noah Paxton <30115747@nescol.ac.uk>
2023-11-01 11:51:55 +0000

    new file

---
+++ src/classarray.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class classarray {
+    public static void main(String[] args) {
+        String[] cls = {"Christopher", "Susan", "Bill", "Satya","Noah"};
+        System.out.println(cls[0]);
+        System.out.println(cls[4]);
+        System.out.println(cls.length);
+    }
+}
